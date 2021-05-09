b171ecc4d267172b81326dfa4a64299970e04cf7
basharckr <basharpmr@gmail.com>
2021-05-09 11:46:38 +0530

    task model created in project leader sider

--- projectlead/models.py
+++ projectlead/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+
+class Tasks(models.Model):
+    task = models.CharField(max_length=100, blank=True, null=True)
+    employee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    task_complete = models.BooleanField(default=False)
