6c6961bb305b13039962e540361e3c821516ca40
basharckr <basharpmr@gmail.com>
2021-05-20 13:40:29 +0530

    deleted dummy contents

--- projectlead/models.py
+++ projectlead/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from hr.models import Project
 
 User = get_user_model()
 # Create your models here.
@@ -8,3 +9,6 @@ class Tasks(models.Model):
     task = models.TextField(max_length=100, blank=True, null=True)
     employee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     task_complete = models.BooleanField(default=False)
+
+
+
